ec18f8d7bcb145eefbade665b47212f4567a50c0
Adam Carr <adam.carr@gmail.com>
2011-08-14 16:22:52 -0400

    General updates.

--- DotNetDesign.EntityFramework/Properties/AssemblyInfo.cs
+++ DotNetDesign.EntityFramework/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("DotNetDesign.EntityFramework")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Library that provides smart entity structure for versioning and persisting.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("DotNetDesign")]
 [assembly: AssemblyProduct("DotNetDesign.EntityFramework")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.1.0")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
