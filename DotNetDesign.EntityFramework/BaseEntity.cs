8ee6cd046e857bd0cf9249addba6356829cbc761
Adam Carr <adam.carr@gmail.com>
2011-08-13 20:13:55 -0400

    Code clean-up

--- DotNetDesign.EntityFramework/BaseEntity.cs
+++ DotNetDesign.EntityFramework/BaseEntity.cs
@@ -4,7 +4,6 @@ using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Linq.Expressions;
-using System.Reflection;
 
 namespace DotNetDesign.EntityFramework
 {
