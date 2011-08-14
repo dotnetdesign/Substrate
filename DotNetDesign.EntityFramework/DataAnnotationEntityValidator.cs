d465ee30daea1f590be7a26c16aa680be0df736e
Adam Carr <adam.carr@gmail.com>
2011-08-13 20:12:22 -0400

    Added DataAnnotationEntityValidator.cs to automate the validation of data annotation validation attributes that are added to properties on TEntity and TEntityData types.

---
+++ DotNetDesign.EntityFramework/DataAnnotationEntityValidator.cs
@@ -0,0 +1,71 @@
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+
+namespace DotNetDesign.EntityFramework
+{
+    /// <summary>
+    /// Validates property data annotation validation attributes on TEntity and TEntityData.
+    /// </summary>
+    /// <typeparam name="TEntity">The type of the entity.</typeparam>
+    /// <typeparam name="TEntityData">The type of the entity data.</typeparam>
+    /// <typeparam name="TId">The type of the id.</typeparam>
+    /// <typeparam name="TEntityRepository">The type of the entity repository.</typeparam>
+    public class DataAnnotationEntityValidator<TEntity, TEntityData, TId, TEntityRepository> :
+        IEntityValidator<TEntity, TEntityData, TId, TEntityRepository>
+        where TEntity : class, IEntity<TEntity, TId, TEntityData, TEntityRepository>, TEntityData
+        where TEntityData : class, IEntityData<TEntityData, TEntity, TId, TEntityRepository>
+        where TEntityRepository : class, IEntityRepository<TEntityRepository, TEntity, TId, TEntityData>
+    {
+        #region IEntityValidator<TEntity,TEntityData,TId,TEntityRepository> Members
+
+        /// <summary>
+        /// Validates the specified entity.
+        /// </summary>
+        /// <param name="entity">The entity.</param>
+        /// <returns></returns>
+        public IEnumerable<ValidationResult> Validate(TEntity entity)
+        {
+            var validationResults = new List<ValidationResult>();
+
+            validationResults.AddRange(ValidateType<TEntityData>(entity));
+            validationResults.AddRange(ValidateType<TEntity>(entity));
+
+            return validationResults;
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Validates the type.
+        /// </summary>
+        /// <typeparam name="TValidatableType">The type of the validatable type.</typeparam>
+        /// <param name="entity">The entity.</param>
+        /// <returns></returns>
+        private IEnumerable<ValidationResult> ValidateType<TValidatableType>(TEntity entity)
+        {
+            return from property in typeof(TValidatableType).GetProperties()
+                   from validationAttribute in
+                       property.GetCustomAttributes(typeof (ValidationAttribute), true).OfType<ValidationAttribute>()
+                   where !validationAttribute.IsValid(property.GetValue(entity, null))
+                   select
+                       new ValidationResult(
+                       validationAttribute.FormatErrorMessage(GetDisplayName(property)),
+                       new[] {property.Name});
+        }
+
+        /// <summary>
+        /// Gets the display name.
+        /// </summary>
+        /// <param name="propertyInfo">The property info.</param>
+        /// <returns></returns>
+        protected virtual string GetDisplayName(PropertyInfo propertyInfo)
+        {
+            return
+                ((DisplayNameAttribute)
+                 propertyInfo.GetCustomAttributes(typeof (DisplayNameAttribute), true).SingleOrDefault()).DisplayName;
+        }
+    }
+}
