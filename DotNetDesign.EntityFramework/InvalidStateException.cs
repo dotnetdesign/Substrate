073dfef52284f45ba9e0a8f0eb1e025b51887838
Adam Carr <adam.carr@gmail.com>
2011-09-29 00:25:12 -0400

    Added state machine support

---
+++ DotNetDesign.EntityFramework/InvalidStateException.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace DotNetDesign.EntityFramework
+{
+    /// <summary>
+    /// Invalid state exception
+    /// </summary>
+    /// <typeparam name="TState">The type of the state.</typeparam>
+    [Serializable]
+    public class InvalidStateException<TState> : Exception
+    {
+        private const string ERROR_MESSAGE_FORMAT =
+            "Change from state {0} to state {1} is not allowed. Allowed states, [{2}].";
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="InvalidStateException&lt;TState&gt;"/> class.
+        /// </summary>
+        /// <param name="currentState">State of the current.</param>
+        /// <param name="targetState">State of the target.</param>
+        /// <param name="allowedStates">The allowed states.</param>
+        public InvalidStateException(TState currentState, TState targetState, IEnumerable<TState> allowedStates)
+            : base(string.Format(ERROR_MESSAGE_FORMAT, currentState, targetState, string.Join(", ", allowedStates)))
+        {
+            CurrentState = currentState;
+            TargetState = targetState;
+            AllowedStates = allowedStates;
+        }
+
+        /// <summary>
+        /// Gets the state of the current.
+        /// </summary>
+        /// <value>
+        /// The state of the current.
+        /// </value>
+        public TState CurrentState { get; private set; }
+
+        /// <summary>
+        /// Gets the state of the target.
+        /// </summary>
+        /// <value>
+        /// The state of the target.
+        /// </value>
+        public TState TargetState { get; private set; }
+
+        /// <summary>
+        /// Gets the allowed states.
+        /// </summary>
+        public IEnumerable<TState> AllowedStates { get; private set; }
+    }
+}
---
+++ DotNetDesign.EntityFramework/StateChangeEventArgs.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Data;
+
+namespace DotNetDesign.EntityFramework
+{
+    /// <summary>
+    /// State Change Event Args
+    /// </summary>
+    public class StateChangeEventArgs<TState> : EventArgs
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="StateChangeEventArgs&lt;TState&gt;"/> class.
+        /// </summary>
+        /// <param name="originalState">State of the original.</param>
+        /// <param name="newState">The new state.</param>
+        public StateChangeEventArgs(TState originalState, TState newState)
+        {
+            OriginalState = originalState;
+            NewState = newState;
+        }
+
+        /// <summary>
+        /// Gets the state of the original.
+        /// </summary>
+        /// <value>
+        /// The state of the original.
+        /// </value>
+        public TState OriginalState { get; private set; }
+
+        /// <summary>
+        /// Gets the new state.
+        /// </summary>
+        public TState NewState { get; private set; }
+
+        /// <summary>
+        /// Toes the event args.
+        /// </summary>
+        /// <param name="stateChangeEventArgs">The <see cref="DotNetDesign.EntityFramework.StateChangeEventArgs&lt;TState&gt;"/> instance containing the event data.</param>
+        /// <returns></returns>
+        public static EventArgs ToEventArgs(StateChangeEventArgs<TState> stateChangeEventArgs)
+        {
+            return stateChangeEventArgs;
+        }
+    }
+}
