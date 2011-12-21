4d850ad80bc605ed846f123aaf5035eb254a229b
Adam Carr <adam.carr@gmail.com>
2011-12-20 19:16:06 -0500

    Added xml documentation for ScopeLogger

--- DotNetDesign.EntityFramework/ScopeLogger.cs
+++ DotNetDesign.EntityFramework/ScopeLogger.cs
@@ -7,11 +7,18 @@ using Common.Logging;
 
 namespace DotNetDesign.EntityFramework
 {
+    /// <summary>
+    /// Disposable object that will write a trace message when created and when disposed.
+    /// </summary>
     public class ScopeLogger : IDisposable
     {
         private readonly ILog _logger;
         private readonly string _methodName;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ScopeLogger"/> class.
+        /// </summary>
+        /// <param name="logger">The logger.</param>
         public ScopeLogger(ILog logger)
         {
             _logger = logger;
@@ -19,6 +26,9 @@ namespace DotNetDesign.EntityFramework
             _logger.Trace("Enter " + _methodName);
         }
 
+        /// <summary>
+        /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+        /// </summary>
         public void Dispose()
         {
             _logger.Trace("Exit " + _methodName);
