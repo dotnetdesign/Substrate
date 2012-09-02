290116de5cf1853cf857756ac13b8b9a8f9972e9
Adam Carr <adam.carr@gmail.com>
2012-09-02 15:08:15 -0400

    Added example project to solution

---
+++ src/DotNetDesign.Substrate.Example.ConsoleClient/Program.cs
@@ -0,0 +1,73 @@
+﻿using Autofac;
+using Autofac.Integration.Wcf;
+using DotNetDesign.Substrate.Example.Entities;
+using DotNetDesign.Substrate.Example.Entities.Autofac;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+
+namespace DotNetDesign.Substrate.Example.ConsoleClient
+{
+    class Program
+    {
+        private const string ROOT_URI = "net.tcp://localhost:8080/";
+
+        static void Main(string[] args)
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterModule(new EntitiesModule());
+            RegisterNetTcpService<IUserRepositoryService>(builder, ROOT_URI + "UserRepositoryService");
+            builder.RegisterType<DictionaryScopeManager>().As<IScopeManager>().SingleInstance();
+
+            using (var container = builder.Build())
+            {
+                using (var lifetimeScope = container.BeginLifetimeScope())
+                {
+                    var userRepository = lifetimeScope.Resolve<IUserRepository>();
+                    var users = userRepository.GetAll();
+                    if (users.Count() > 0)
+                    {
+                        foreach (var user in users)
+                        {
+                            Console.WriteLine("First Name: {0} | Last Name: {1} | E-mail Address: {2}", user.FirstName, user.LastName, user.EmailAddress);
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("No users");
+
+                        Console.WriteLine("Adding a user");
+
+                        var user = userRepository.GetNew();
+                        user.FirstName = "Adam";
+                        user.LastName = "Carr";
+                        user.EmailAddress = "adam.carr@gmail.com";
+                        user.Save();
+
+                        Console.WriteLine("Added user {0}", user);
+                    }
+                }
+            }
+
+            Console.Read();
+        }
+
+        static void RegisterNetTcpService<TService>(ContainerBuilder builder, string address)
+        {
+            builder.Register(
+                container =>
+                new ChannelFactory<TService>(new NetTcpBinding()
+                {
+                    SendTimeout = new TimeSpan(1, 0, 0),
+                    ReceiveTimeout = new TimeSpan(0, 5, 0)
+                },
+                new EndpointAddress(address))).SingleInstance();
+
+            builder.Register(
+                c => c.Resolve<ChannelFactory<TService>>().CreateChannel())
+                .UseWcfSafeRelease();
+        }
+    }
+}
---
+++ src/DotNetDesign.Substrate.Example.Entities.SqlServer/UserRepositoryService.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DotNetDesign.Common;
+using System.Data;
+
+namespace DotNetDesign.Substrate.Example.Entities.SqlServer
+{
+    public class UserRepositoryService : BaseRepositoryService, IUserRepositoryService
+    {
+        public UserRepositoryService()
+            : base()
+        {
+
+        }
+
+        public void Delete(Guid id, Dictionary<string, string> scopeContext)
+        {
+            using (TraceTopics.SqlServer.Scope())
+            {
+                var usersToDelete = DbContext.Users.Where(x => x.Id == id);
+                foreach (var userData in usersToDelete)
+                {
+                    DbContext.Users.Remove(userData);
+                }
+                DbContext.SaveChanges();
+            }
+        }
+
+        public void DeleteAll(IEnumerable<Guid> ids, Dictionary<string, string> scopeContext)
+        {
+            using (TraceTopics.SqlServer.Scope())
+            {
+                foreach (var userId in ids)
+                {
+                    Delete(userId, scopeContext);
+                }
+            }
+        }
+
+        public IEnumerable<UserData> GetAll(Dictionary<string, string> scopeContext)
+        {
+            using (TraceTopics.SqlServer.Scope())
+            {
+                return from user in DbContext.Users
+                       select user;
+            }
+        }
+
+        public UserData GetById(Guid id, Dictionary<string, string> scopeContext)
+        {
+            using (TraceTopics.SqlServer.Scope())
+            {
+                return (from user in DbContext.Users
+                        where user.Id == id
+                        select user).FirstOrDefault();
+            }
+        }
+
+        public IEnumerable<UserData> GetByIds(IEnumerable<Guid> ids, Dictionary<string, string> scopeContext)
+        {
+            using (TraceTopics.SqlServer.Scope())
+            {
+                return from user in DbContext.Users
+                       where ids.Contains(user.Id)
+                       select user;
+            }
+        }
+
+        public UserData GetVersion(Guid id, int version, Dictionary<string, string> scopeContext)
+        {
+            throw new NotImplementedException();
+        }
+
+        public UserData Save(UserData entityData, Dictionary<string, string> scopeContext)
+        {
+            using (TraceTopics.SqlServer.Scope())
+            {
+                AttachIfNotAttached(entityData, (entityData.Version == 1) ? EntityState.Added : EntityState.Modified);
+                DbContext.SaveChanges();
+                return entityData;
+            }
+        }
+
+        public IEnumerable<UserData> SaveAll(IEnumerable<UserData> entityData, Dictionary<string, string> scopeContext)
+        {
+            using (TraceTopics.SqlServer.Scope())
+            {
+                return entityData.Select(x => Save(x, scopeContext));
+            }
+        }
+    }
+}
